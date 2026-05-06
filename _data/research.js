49d8d74f6e55fa3f6b561fd89183e7cbbc68e821
Mehrnoosh <61428511+MehrnooshE@users.noreply.github.com>
2026-05-06 19:39:57 -0400

    Update research.js added hyperlinks and fundings

--- _data/research.js
+++ _data/research.js
@@ -20,22 +20,32 @@ const RESEARCH = [
   {
     tag: "Biomarkers of Aging",
     title: "Biomarkers of Aging",
-    description:"Moqri Lab is a founding member of the <a href="https://www.agingconsortium.org/" target="_blank">Biomarkers of Aging Consortium</a>, establishing reliable biomarkers of aging for longevity interventions.",
-    link: "https://www.agingconsortium.org/"
+    description:'Moqri Lab is a founding member of the <a href="https://www.agingconsortium.org/" target="_blank">Biomarkers of Aging Consortium</a>, establishing reliable biomarkers of aging for longevity interventions.',
+    link: null,
+      funding: [
+    { name: "Proactive Solutions for Prolonging Resilience (PROSPR)", link: "https://arpa-h.gov/explore-funding/programs/prospr" },
+    { name: "Finding Aging biomarkers by Searching existing Trials (FAST)", link: "https://www.afar.org/fast-initiativem" }
+  ]
   },
   {
     tag: "Biomarkers of Alzheimer's",
     title: "Biomarkers of Alzheimer's",
     description:
-      "Moqri Lab builds and validates blood-based multi-omic biomarkers for brain aging and early detection of Alzheimer's.",
-    link: "https://www.agingconsortium.org/brain-aging"
+      'Moqri Lab build and validate blood-based multi-omic biomarkers for <a href="https://www.agingconsortium.org/" target="_blank">Brain Aging</a> and early detection of Alzheimer\'s.',
+    link: null,
+      funding: [
+    { name: "Proteogenomic Biomarkers for Early Detection of Alzheimer’s Disease", link: "https://curealz.org/research/foundational/biomarkers-diagnostics-studies-of-risk-resilience/proteogenomic-biomarkers-for-early-detection-of-alzheimers-disease/" }
+  ]
   },
   {
     tag: "AI for Patient Outcomes",
     title: "AI for Patient Outcomes",
     description:
-      "Moqri Lab uses large-scale electronic health records (EHR) and multi-omics data from the MGB Biobank to predict patient outcomes and prevent functional decline.",
-    link: "https://www.massgeneralbrigham.org/en/research-and-innovation/participate-in-research/biobank/for-researchers"
+      'Moqri Lab is using large-scale Electronic Health Records (EHR) data and multi-omics data from <a href="https://www.massgeneralbrigham.org/en/research-and-innovation/participate-in-research/biobank/for-researchers" target="_blank">MGB Biobank</a> to predict patient outcomes and prevent functional decline.',
+    link: null,
+      funding: [
+    { name: "MATCH: Advanced Machine Learning AlgoriThm for Compatible Human Leukocyte Antigen", link: "https://arpa-h.gov/explore-funding/awards/4241" }
+  ]
   }
 ];
 
